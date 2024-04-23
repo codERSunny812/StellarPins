55cda758afad4e16a114fd7ebd437208e5e9b72e
codersunny812 <sengersunny448@gmail.com>
2024-04-23 21:04:32 +0530

    routes

--- server/routes/Post.js
+++ server/routes/Post.js
@@ -1,28 +1,32 @@
 import express from 'express'
 import { postModal } from '../models/post.modal.js'
 import { UserModal } from '../models/user.modal.js';
+import { upload } from '../utils/Multer.config.js';
 
 
 
 const Router = express.Router();
 
 
-
 // route to create a  post
+Router.post('/upload',upload.single('file'),async(req,res)=>{
+if(!req.file) return res.status(404).send("no file found")
 
-Router.post('/',async(req,res)=>{
-    const {title , userId} = req.body
-   const createdPost = await postModal.create({
-    caption:title,
-    user:userId
-   });
-    await createdPost.save();
 
-//    user who create the post 
-let user = await UserModal.findOne({_id:userId});
-  user.post.push(createdPost._id);
+const user = await UserModal.findOne({_id:req.session.passport.user});
 
-  await user.save();
+const newPost = await  postModal.create({
+  caption: req.body.caption,
+  imageUrl:req.file.filename,
+  user:user._id
+});
+
+  await user.post(newPost._id);
+
+
+await newPost.save();
+
+
 
   
 
