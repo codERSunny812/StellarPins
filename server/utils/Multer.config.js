76a8446d97cb5533275323ee49a6c579ac06430e
codersunny812 <sengersunny448@gmail.com>
2024-04-23 21:05:23 +0530

    multer configuration

---
+++ server/utils/Multer.config.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path'
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './Public/Image/uploads')
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = uuidv4();
+        cb(null, uniqueName+path.extname(file.originalname));
+    }
+})
+
+ export const upload = multer({ storage: storage })
