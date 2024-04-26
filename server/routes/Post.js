1637e7a4cbf907e1ce6cf52cc862348f886f10f8
codersunny812 <sengersunny448@gmail.com>
2024-04-26 11:44:46 +0530

    routes

--- server/routes/Post.js
+++ server/routes/Post.js
@@ -1,44 +1,65 @@
 import express from "express"
 import { postModal } from "../models/post.modal.js"
-import { UserModal } from "../models/user.modal.js"
+import { userModal } from "../models/user.modal.js"
 import { upload } from "../utils/Multer.config.js"
+import { connectCloudinary } from "../utils/Cloudinary.config.js"
+import fs from 'fs'
 
 const Router = express.Router()
 
 // route to create a  post
 Router.post("/upload", upload.single("file"), async (req, res) => {
-   try {
+  try {
+    if (!req.file) return res.status(404).send("no file found")
+    
+    // storing image  on the cloud
+    const cloudLink = await connectCloudinary(req.file.path,{
+      transformation: [
+        { width: 300, height: 300, crop: "fill" },
+        { radius: "max" }
 
-      if (!req.file) return res.status(404).send("no file found");
-
-      const userId = req.session.passport.user;
-      const user = await UserModal.findById(userId);
-
-      if (!user) {
-         return res.status(404).send("User not found");
-      }
-
-      const newPost = await postModal.create({
-         caption: req.body.caption,
-         imageUrl: req.file.filename,
-         user: userId
-      });
-
-      // Update user's posts array
-      user.posts.push(newPost._id);
-      await user.save();
-
-      res.send("Post created successfully");
-   
-   } catch (error) {
-
-      console.error("Error creating post:", error);
-      res.status(500).send("Internal Server Error");
-      
-   }
- 
+      ]
+    });
 
 
+    fs.unlinkSync(req.file.path);
+
+
+
+
+
+    const userId = req.session.passport.user
+
+    const user = await userModal.findById(userId)
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+
+
+    const newPost = await postModal.create({
+      caption: req?.body?.caption,
+      imageUrl: cloudLink?.secure_url,
+      user: userId,
+    })
+
+    // Update user's posts array
+    user.posts.push(newPost._id)
+
+    await user.save()
+
+    console.log("post created succesfully")
+
+    // console.log(user)
+
+    res.redirect('/profile')
+
+  }
+  catch (error) {
+    console.error("Error creating post:", error)
+    res.status(500).send("Internal Server Error")
+  }
 })
 
-export default Router
+export default Router;
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,68 +1,81 @@
 import express from "express"
-import { UserModal } from "../models/user.modal.js";
-import bcryptjs from 'bcryptjs'
-import passport from "passport";
+import { userModal } from "../models/user.modal.js"
+import bcryptjs from "bcryptjs"
+import passport from "passport"
+import { upload } from "../utils/Multer.config.js"
+import { connectCloudinary } from "../utils/Cloudinary.config.js"
+import fs from 'fs'
 
-const Router = express.Router();
-
-
+const Router = express.Router()
 
 // route to create a user
-Router.post('/register',  async(req, res, next)=> {
+Router.post("/register", upload.single('photo') ,async (req, res, next) => {
 
-    console.log(req.body);
-
-    const {userName , email , password , fullName} = req.body;
-    
-    if ( !userName || !email || !password || !fullName ){
-        res.status(200).send("All fields are required");
-    }
-
-    const userAlready =await UserModal.findOne({email});
-
-    if(userAlready){
-        return res.status(200).send('User already exists');
-    }
+try {
 
 
-    // it will hashed the password
-    const hashedPassword =  await bcryptjs.hash(password,10);
+  // uploading the image in the cloudinary
+  const cloudinaryLink = await connectCloudinary(req.file.path, {
+    transformation: [
+      { width: 300, height: 300, crop: "fill" },
+      { radius: "max" }
 
+    ]
+  });
 
-    //create the user 
-    const user =await UserModal.create(({
-        userName: userName,
-        email: email,
-        password:hashedPassword,
-        fullName: fullName,
-        
-    }));
+  // deleting the file from the local
+  fs.unlinkSync(req.file.path);
 
-    await user.save()
+   
+  const { userName, email, password, fullName } = req.body
 
+  if (!userName || !email || !password || !fullName) {
+    res.status(200).send("All fields are required")
+  }
 
-  res.redirect('/login');
+  const userAlready = await userModal.findOne({ email })
 
-});
+  if (userAlready) {
+    return res.status(200).send("User already exists")
+  }
 
+  // hashing  the password before saving it into database
+  const hashedPassword = await bcryptjs.hash(password, 10)
 
-Router.post('/login', passport.authenticate('local', {
-    successRedirect: '/feed', // Change this to your success route
-    failureRedirect: '/login',
-    failureFlash:true
-}));
+  //create the user
+  const user = await userModal.create({
+    userName: userName,
+    email: email,
+    password: hashedPassword,
+    fullName: fullName,
+    avtar: cloudinaryLink.secure_url,
+  });
 
+  await user.save()
 
-Router.get('/alluser',(req,res)=>{
-    res.send("this is the all user");
+  res.redirect("/login")
+
+}catch (error) {
+  console.log("failed to register the user");
+}
+
 })
 
-Router.post('/logout',(req,res,next)=>{
-req.logout((err)=>{
-    if (err) { return next(err); }
-    res.redirect('/');
+Router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/feed", // Change this to your success route
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+)
+
+Router.post("/logout", (req, res, next) => {
+req.logOut((error)=>{
+  if(error) return  next(error);
+  
+  res.redirect('/');
 })
 })
 
-
-export default Router;
+export default Router
